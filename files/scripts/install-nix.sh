28e457cb72a75b7838df7aeddd8b343f777b79e6
certifiedfoolio <156134535+cherry-os@users.noreply.github.com>
2025-03-16 23:13:25 +0000

    fix: use sed

--- files/scripts/install-nix.sh
+++ files/scripts/install-nix.sh
@@ -9,25 +9,4 @@ sh <(curl -L https://nixos.org/nix/install) --daemon --yes
 
 mkdir -p $NIX_FACTORY_INSTALL_PATH
 mv /nix $NIX_FACTORY_INSTALL_PATH
-rm -f /etc/systemd/system/nix-daemon.{service,socket}
-set -x
-find / -name 'nix-daemon.{service,socket}' 2>/dev/null
-set +x
-
-semanage fcontext -a -t etc_t '/nix/store/[^/]+/etc(/.*)?'
-semanage fcontext -a -t lib_t '/nix/store/[^/]+/lib(/.*)?'
-semanage fcontext -a -t systemd_unit_file_t '/nix/store/[^/]+/lib/systemd/system(/.*)?'
-semanage fcontext -a -t man_t '/nix/store/[^/]+/man(/.*)?'
-semanage fcontext -a -t bin_t '/nix/store/[^/]+/s?bin(/.*)?'
-semanage fcontext -a -t usr_t '/nix/store/[^/]+/share(/.*)?'
-semanage fcontext -a -t var_run_t '/nix/var/nix/daemon-socket(/.*)?'
-semanage fcontext -a -t usr_t '/nix/var/nix/profiles(/per-user/[^/]+)?/[^/]+'
-semanage fcontext -a -t etc_t '/var/nix/store/[^/]+/etc(/.*)?'
-semanage fcontext -a -t lib_t '/var/nix/store/[^/]+/lib(/.*)?'
-semanage fcontext -a -t systemd_unit_file_t '/var/nix/store/[^/]+/lib/systemd/system(/.*)?'
-semanage fcontext -a -t man_t '/var/nix/store/[^/]+/man(/.*)?'
-semanage fcontext -a -t bin_t '/var/nix/store/[^/]+/s?bin(/.*)?'
-semanage fcontext -a -t usr_t '/var/nix/store/[^/]+/share(/.*)?'
-semanage fcontext -a -t var_run_t '/var/nix/var/nix/daemon-socket(/.*)?'
-semanage fcontext -a -t usr_t '/var/nix/var/nix/profiles(/per-user/[^/]+)?/[^/]+'
-restorecon -RF /nix
+sed -i '/^\[Unit\]/a BindsTo=nix.mount\nAfter=nix.mount' /etc/systemd/system/nix-daemon.{service,socket}
