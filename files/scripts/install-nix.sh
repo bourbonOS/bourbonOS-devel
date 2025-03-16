927f7c52d28ac780eb83888f3940fe776645e704
certifiedfoolio <156134535+cherry-os@users.noreply.github.com>
2025-03-16 21:50:57 +0000

    feat: bump to 123.6.1-devel

---
+++ files/scripts/install-nix.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+NIX_FACTORY_INSTALL_PATH="/usr/share/factory/var/nix"
+
+sh -c "$(curl -L https://nixos.org/nix/install)" --daemon --yes
+
+mv /nix $NIX_FACTORY_INSTALL_PATH
+mkdir /nix
+rm -f /etc/systemd/system/nix-daemon.{service,socket}
+cp $NIX_FACTORY_INSTALL_PATH/var/nix/profiles/default/lib/systemd/system/nix-daemon.{service,socket} /etc/systemd/system/
+systemctl enable nix-daemon.socket
+
+semanage fcontext -a -t etc_t '/nix/store/[^/]+/etc(/.*)?'
+semanage fcontext -a -t lib_t '/nix/store/[^/]+/lib(/.*)?'
+semanage fcontext -a -t systemd_unit_file_t '/nix/store/[^/]+/lib/systemd/system(/.*)?'
+semanage fcontext -a -t man_t '/nix/store/[^/]+/man(/.*)?'
+semanage fcontext -a -t bin_t '/nix/store/[^/]+/s?bin(/.*)?'
+semanage fcontext -a -t usr_t '/nix/store/[^/]+/share(/.*)?'
+semanage fcontext -a -t var_run_t '/nix/var/nix/daemon-socket(/.*)?'
+semanage fcontext -a -t usr_t '/nix/var/nix/profiles(/per-user/[^/]+)?/[^/]+'
+semanage fcontext -a -t etc_t '/var/nix/store/[^/]+/etc(/.*)?'
+semanage fcontext -a -t lib_t '/var/nix/store/[^/]+/lib(/.*)?'
+semanage fcontext -a -t systemd_unit_file_t '/var/nix/store/[^/]+/lib/systemd/system(/.*)?'
+semanage fcontext -a -t man_t '/var/nix/store/[^/]+/man(/.*)?'
+semanage fcontext -a -t bin_t '/var/nix/store/[^/]+/s?bin(/.*)?'
+semanage fcontext -a -t usr_t '/var/nix/store/[^/]+/share(/.*)?'
+semanage fcontext -a -t var_run_t '/var/nix/var/nix/daemon-socket(/.*)?'
+semanage fcontext -a -t usr_t '/var/nix/var/nix/profiles(/per-user/[^/]+)?/[^/]+'
+restorecon -RF /nix
