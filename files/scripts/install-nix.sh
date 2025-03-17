a8913a06a81213d16d1632e442d360ec438e9760
certifiedfoolio <156134535+cherry-os@users.noreply.github.com>
2025-03-17 15:51:37 +0000

    fix: create nix after mv

--- files/scripts/install-nix.sh
+++ files/scripts/install-nix.sh
@@ -3,8 +3,13 @@
 set -oue pipefail
 NIX_FACTORY_INSTALL_PATH="/usr/share/factory/var"
 
-mkdir -m 0755 /nix
-chown root /nix
+cre_nix() {
+    mkdir -m 0755 /nix
+    chown root /nix
+}
+
+cre_nix
 sh <(curl -L https://nixos.org/nix/install) --daemon --yes
 mkdir -p $NIX_FACTORY_INSTALL_PATH
 mv /nix $NIX_FACTORY_INSTALL_PATH
+cre_nix
