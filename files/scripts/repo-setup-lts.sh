1950becfb8802c5282ac009a127fb8060e1990bd
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2025-03-20 23:04:05 +0000

    fix: enable CRB/EPEL before pkg inst

--- files/scripts/repo-setup-lts.sh
+++ files/scripts/repo-setup-lts.sh
@@ -2,7 +2,6 @@
 
 set -oue pipefail
 
-dnf -y config-manager --set-enabled crb
 dnf -y install epel-release
+crb enable
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-10
-dnf -y config-manager --add-repo https://copr.fedorainfracloud.org/coprs/chronos/cherries/repo/epel-10/chronos-cherries-epel-10.repo
