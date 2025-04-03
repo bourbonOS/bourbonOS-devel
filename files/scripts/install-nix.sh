8184d06c0ce97bdf00c59be8a3b4e4d8a624069f
boostyislame <leeamringordespi@gmail.com>
2025-04-03 19:00:30 +0000

    test push

---
+++ files/scripts/install-nix.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+NIX_FACTORY_INSTALL_PATH="/usr/share/.fhs-noncompliant/"
+
+mkdir -m 0755 /nix
+chown root /nix
+sh <(curl -L https://nixos.org/nix/install) --daemon --yes
+mkdir -p $NIX_FACTORY_INSTALL_PATH
+mv /nix $NIX_FACTORY_INSTALL_PATH
