089e543b46638add78b4fa0d5821644142b6bc6e
boostyislame <leeamringordespi@gmail.com>
2025-04-03 22:28:45 +0000

    feat: add nix to /usr/local

--- files/scripts/install-nix.sh
+++ files/scripts/install-nix.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 set -oue pipefail
-NIX_FACTORY_INSTALL_PATH="/usr/share/.fhs-noncompliant/"
 
 mkdir -m 0755 /nix
 chown root /nix
 sh <(curl -L https://nixos.org/nix/install) --daemon --yes
-mkdir -p $NIX_FACTORY_INSTALL_PATH
-mv /nix $NIX_FACTORY_INSTALL_PATH
--- files/scripts/unprotectsudo.sh
+++ files/scripts/unprotectsudo.sh
@@ -3,7 +3,3 @@
 set -oue pipefail
 
 rm /etc/dnf/protected.d/sudo.conf
-set +x
-ls /home
-ls /var/home
-set -x
