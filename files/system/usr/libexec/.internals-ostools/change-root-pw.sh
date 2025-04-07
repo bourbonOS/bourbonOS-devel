8cc5380dbf7f627be9350fbe6f1dafd2ac8bbbed
boostyislame <leeamringordespi@gmail.com>
2025-04-07 06:49:04 +0000

    fix: groupdel wheel

--- files/system/usr/libexec/.internals-ostools/change-root-pw.sh
+++ files/system/usr/libexec/.internals-ostools/change-root-pw.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+for user in $(getent group wheel | cut -d: -f4 | tr ',' ' '); do
+    gpasswd -d "$user" wheel
+done
+
+groupdel wheel
+
 password="$(openssl rand -base64 48)"
 echo "root:$password" | chpasswd
 echo "Password changed successfully"
