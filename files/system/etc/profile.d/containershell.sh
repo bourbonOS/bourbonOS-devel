916eed52a73c8d76e517da5ee7ec98e6b7c30532
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2025-04-07 21:26:41 -0700

    fix: incorrect path

--- files/system/etc/profile.d/containershell.sh
+++ files/system/etc/profile.d/containershell.sh
@@ -7,7 +7,7 @@ if [[ "$TERM" == "xterm-256color" && -d /var/home/$USER ]]; then
         echo "Don't worry! This wont take long."
         echo "The shell will open when we are done."
         mkdir -p $HOME/.config/containerconf
-        exec distrobox-assemble create --file /etc/containerconf/configs.d/.cherry/cherry.ini
+        exec distrobox-assemble create --file /etc/containerconf/.cherry/cherry.ini
     else
         echo "Starting your shell...."
         exec distrobox-enter cherry-cli
