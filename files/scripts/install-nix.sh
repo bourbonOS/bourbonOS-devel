6fe7ee171f00fd3d8412fd0bb1179afcdc3af4c1
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2025-04-10 23:54:06 -0700

    fix: systemd depends

--- files/scripts/install-nix.sh
+++ files/scripts/install-nix.sh
@@ -6,4 +6,10 @@ sh <(curl -L https://nixos.org/nix/install) --daemon --yes
 
 mkdir /etc/.nix-mount-temp
 mv /nix /etc/.nix-mount-temp
-mkdir /nix
+mkdir /nix /usr/lib/systemd/system/gdm.service.d/override.conf
+
+cat <<EOF > /usr/lib/systemd/system/gdm.service.d/override.conf
+[Unit]
+After=homefs-setup.service
+Requires=homefs-setup.service
+EOF
