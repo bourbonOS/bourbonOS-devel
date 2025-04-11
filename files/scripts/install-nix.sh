998bdb0e05964f1eda9ed94590b9a5547b8fa067
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2025-04-10 23:40:44 -0700

    testing

--- files/scripts/install-nix.sh
+++ files/scripts/install-nix.sh
@@ -6,6 +6,7 @@ dnf -y install fuse3
 truncate -s 1T /nix.img
 mkfs.ext4 -F /nix.img
 mkdir -p /nix-fs
+fusermount --help
 ext4fuse /nix.img /nix-fs
 sh <(curl -L https://nixos.org/nix/install) --daemon --yes
 fusermount -u /nix-fs
