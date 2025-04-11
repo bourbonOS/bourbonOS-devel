73d8ede30c14970fd75f57294a8bacad394ed394
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2025-04-11 00:13:10 -0700

    fix: override dir

--- files/scripts/install-nix.sh
+++ files/scripts/install-nix.sh
@@ -6,9 +6,9 @@ sh <(curl -L https://nixos.org/nix/install) --daemon --yes
 
 mkdir /etc/.nix-mount-temp
 mv /nix /etc/.nix-mount-temp
-mkdir /nix /usr/lib/systemd/system/gdm.service.d/override.conf
+mkdir /nix /etc/systemd/system/gdm.service.d/
 
-cat <<EOF > /usr/lib/systemd/system/gdm.service.d/override.conf
+cat <<EOF > /etc/systemd/system/gdm.service.d/override.conf
 [Unit]
 After=homefs-setup.service
 Requires=homefs-setup.service
