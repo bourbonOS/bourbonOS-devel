1dd796eb95cf51079397e461ece8b0756867ad64
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2025-04-14 10:20:32 -0700

    chore(sysd): move to file

--- files/scripts/install-nix.sh
+++ files/scripts/install-nix.sh
@@ -4,9 +4,3 @@ set -oue pipefail
 
 mkdir /nix
 sh <(curl -L https://nixos.org/nix/install) --daemon --yes
-
-cat <<EOF > /etc/systemd/system/gdm.service.d/override.conf
-[Unit]
-After=mount-loopfs.service
-BindsTo=mount-loopfs.service
-EOF
