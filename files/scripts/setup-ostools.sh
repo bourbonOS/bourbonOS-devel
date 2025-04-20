826b3560430c4aecc72efff420911d37008f5181
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2025-04-20 12:01:19 -0700

    test: try /sysroot

--- files/scripts/setup-ostools.sh
+++ files/scripts/setup-ostools.sh
@@ -3,7 +3,7 @@
 set -oue pipefail
 set +x
 
-ls /ostree -lah
+ls /sysroot/ -lah
 
 echo "session    required    pam_exec.so    /usr/libexec/homefs/manage_homedir" >> /etc/pam.d/common-session
 echo "/var/ostools/nix  /nix  none  bind  0  0" >> /etc/fstab
