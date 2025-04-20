62b2cdd24cb0bc2d41ce1b63b22ef56f6e8cedb3
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2025-04-20 11:55:56 -0700

    test: try /ostree

--- files/scripts/add-pam-homefs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -oue pipefail
-
-echo "session    required    pam_exec.so    /usr/libexec/homefs/manage_homedir" >> /etc/pam.d/common-session
---
+++ files/scripts/setup-ostools.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+echo "session    required    pam_exec.so    /usr/libexec/homefs/manage_homedir" >> /etc/pam.d/common-session
+echo "/var/ostools/nix  /nix  none  bind  0  0" >> /etc/fstab
+
+mkdir /ostree/deploy/fedora/var/ostools/{,homefs}
