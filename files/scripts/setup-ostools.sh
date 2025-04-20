522bd5d3f05090b217ba0ff6169ba2331508b467
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2025-04-20 19:21:42 +0000

    Revert "test: try /"
    This reverts commit df0f602f3830aae07bdc4a6b05174d604b87df86.
    
    fix: undo all

--- files/scripts/setup-ostools.sh
+++ files/scripts/setup-ostools.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 set -oue pipefail
-set +x
-
-ls / -lah
 
 echo "session    required    pam_exec.so    /usr/libexec/homefs/manage_homedir" >> /etc/pam.d/common-session
 echo "/var/ostools/nix  /nix  none  bind  0  0" >> /etc/fstab
