87fcbd9a49979cf59f7a29b12c244d32ddfa0078
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2025-04-21 00:48:21 +0000

    feat: remove unecessary conf file

--- files/system/etc/profile.d/containershell.sh
+++ files/system/etc/profile.d/containershell.sh
@@ -1,34 +1,23 @@
 #!/bin/bash
 
-root_check() {
-    if getent group wheel > /dev/null 2>&1; then
-        echo "Please set your root password in a few seconds."
-        echo "Be careful to not lose this password, otherwise"
-        echo "you will lose root access entirely."
-        sleep 3
-        passwd root
-        grep "^wheel:" /etc/group | cut -d':' -f4 | while IFS=',' read -r user; do
-            gpasswd --delete "$user" wheel
-        done
-        groupdel wheel
-    fi
-}
-
 cshell() {
     if [[ ! -d "$HOME/.config/containerconf" ]]; then
         clear
         echo "Hello there! Welcome to bourbonOS!"
-        echo "You may (or may not) be asked for your password in a few seconds."
+        echo "We need to do some post-setup for you to access your shell."
+        echo "You will be asked for your password in a few seconds. Be ready!"
         sleep 3
-        pkexec root_check
+        pkexec /usr/libexec/ostools/remove_wheel
         echo "Now, for you to access your shell, we need to set up the default container."
         echo "Don't worry! This wont take long."
         echo "The shell will open when we are done."
+        sleep 1
         mkdir -p $HOME/.config/containerconf
-        clear && exec distrobox-assemble create --file /etc/containerconf/.cherry/cherry.ini
+        /usr/libexec/ostools/loadingbar "Hello there, $USER!" "We are setting up your shell... this might take a second." "distrobox-assemble create --file /etc/containerconf/.cherry/cherry.ini"
+        exec distrobox-enter cherry-cli
     else
         clear
-        echo "Starting your shell...."
+        printf "Starting your shell...\n\n"
         exec distrobox-enter cherry-cli
     fi
 }
@@ -37,10 +26,10 @@ if [[ -d /var/home/$USER ]]; then
     if [[ "$TERM" == "xterm-256color" ]]; then
         cshell
     elif [[ "$TERM" == "linux" ]]; then
-        echo "Would you like to enter debug mode? (access to host shell)"
+        printf "Would you like to enter debug mode? (access to host shell)\n\n"
         read -p "y/n: " debug_ask
         if [[ "$debug_ask" == "y" ]]; then
-            exec PS1='[\u@\h-baseos][\W] \\$ ' sh
+            exec sh
         else
             cshell
         fi
