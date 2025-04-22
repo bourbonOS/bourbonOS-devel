0f2063a8d269a57c5f414a53b640fe75659d6499
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2025-04-22 03:11:46 +0000

    fix: move nix mount to independent mount unit

--- files/scripts/setup-ostools.sh
+++ files/scripts/setup-ostools.sh
@@ -4,6 +4,5 @@ set -oue pipefail
 
 echo "session    required    pam_exec.so    type=open_session    /usr/libexec/homefs/manage_homedir" >> /etc/pam.d/system-auth
 echo "session    required    pam_exec.so    type=close_session    /usr/libexec/homefs/manage_homedir" >> /etc/pam.d/system-auth
-echo "overlay  /nix  overlay  workdir=/var/usrlocal/nixfs/work,upperdir=/var/usrlocal/nixfs/upper,lowerdir=/usr/nix  0  0" >> /etc/fstab
 
 echo "$(date +%s)" > /etc/last_update_run
