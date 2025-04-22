58d4cb7d2de83fb02e89c8f494570261e2f80126
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2025-04-21 19:58:20 -0700

    fix: use system-auth

--- files/scripts/setup-ostools.sh
+++ files/scripts/setup-ostools.sh
@@ -2,7 +2,8 @@
 
 set -oue pipefail
 
-echo "session    required    pam_exec.so    /usr/libexec/homefs/manage_homedir" >> /etc/pam.d/common-session
+echo "session    required    pam_exec.so    type=open_session    /usr/libexec/homefs/manage_homedir" >> /etc/pam.d/system-auth
+echo "session    required    pam_exec.so    type=close_session    /usr/libexec/homefs/manage_homedir" >> /etc/pam.d/system-auth
 echo "overlay  /nix  overlay  workdir=/var/usrlocal/nixfs/work,upperdir=/var/usrlocal/nixfs/upper,lowerdir=/usr/nix  0  0" >> /etc/fstab
 
 echo "$(date +%s)" > /etc/last_update_run
