4fb8d584a10f727addf0be558b5a325bc334d7d5
certifiedfoolio <mastersvanity@gmail.com>
2025-04-23 23:23:36 -0700

    fix: incorrect script path

--- files/scripts/quirks.sh
+++ files/scripts/quirks.sh
@@ -6,10 +6,10 @@ echo "$(date +%s)" > /etc/last_update_run
 chmod +x -R /tmp/misc/*
 
 if cat /etc/os-release | grep 'ID="fedora"' > /dev/null; then
-    /tmp/misc/image-info-stable.sh
+    /tmp/image-info-stable.sh
 elif cat /etc/os-release | grep 'ID="centos"' > /dev/null; then
-    /tmp/misc/image-info-lts.sh
-    /tmp/misc/repo-setup-lts.sh
+    /tmp/image-info-lts.sh
+    /tmp/repo-setup-lts.sh
 else
     echo "Unknown OS type. Abort."
     exit 1
