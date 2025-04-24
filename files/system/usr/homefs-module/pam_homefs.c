a2b59291188b0b1fb14d95a13397443cf9d24cc9
certifiedfoolio <mastersvanity@gmail.com>
2025-04-24 13:48:04 -0700

    fix: wait for process to exit before continuing

--- files/system/usr/homefs-module/pam_homefs.c
+++ files/system/usr/homefs-module/pam_homefs.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <syslog.h>
+#include <sys/wait.h>
 
 #define OPEN_CMD "/usr/libexec/homefs/manage_homedir --mount"
 #define CLOSE_CMD "/usr/libexec/homefs/manage_homedir --umount"
@@ -33,7 +34,29 @@ int pam_sm_open_session(pam_handle_t *pamh, int flags,
             setenv("PAM_USER", retrieved_user, 1);
             syslog(LOG_INFO, "PAM_USER set successfully", retrieved_user);
             syslog(LOG_INFO, "Attempting to mount HomeFS.", OPEN_CMD);
-            system(OPEN_CMD);
+            int status;
+            pid_t pid = fork();
+            if (pid == -1) {
+                syslog(LOG_ERR, "Failed to fork process.");
+                closelog();
+                return PAM_SESSION_ERR;
+            } else if (pid == 0) {
+                execl("/bin/sh", "sh", "-c", OPEN_CMD, (char *)NULL);
+                syslog(LOG_ERR, "Failed to mount! Error: %s", OPEN_CMD);
+                exit(EXIT_FAILURE);
+            } else {
+                if (waitpid(pid, &status, 0) == -1) {
+                    syslog(LOG_ERR, "Error waiting for mount.");
+                    closelog();
+                    return PAM_SESSION_ERR;
+                }
+                if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
+                    syslog(LOG_ERR, "Mount failed with exit status %d!", WEXITSTATUS(status));
+                    closelog();
+                    return PAM_SESSION_ERR;
+                }
+                syslog(LOG_INFO, "Mount succeeded.");
+            }
         } else {
              syslog(LOG_ERR, "Could not set PAM_USER.", pam_strerror(pamh, retval));
              closelog();
@@ -66,7 +89,29 @@ int pam_sm_close_session(pam_handle_t *pamh, int flags,
             setenv("PAM_USER", retrieved_user, 1);
             syslog(LOG_INFO, "PAM_USER set successfully.", retrieved_user);
             syslog(LOG_INFO, "Attempting to unmount HomeFS.", CLOSE_CMD);
-            system(CLOSE_CMD);
+            int status;
+            pid_t pid = fork();
+            if (pid == -1) {
+                syslog(LOG_ERR, "Failed to fork process.");
+                closelog();
+                return PAM_SESSION_ERR;
+            } else if (pid == 0) {
+                execl("/bin/sh", "sh", "-c", CLOSE_CMD, (char *)NULL);
+                syslog(LOG_ERR, "Failed to unmount!! Error: %s", CLOSE_CMD);
+                exit(EXIT_FAILURE);
+            } else {
+                if (waitpid(pid, &status, 0) == -1) {
+                    syslog(LOG_ERR, "Error waiting for unmount.");
+                    closelog();
+                    return PAM_SESSION_ERR;
+                }
+                if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
+                    syslog(LOG_ERR, "Unmount failed with exit status %d!!", WEXITSTATUS(status));
+                    closelog();
+                    return PAM_SESSION_ERR;
+                }
+                syslog(LOG_INFO, "Unmount succeeded.");
+            }
         } else {
              syslog(LOG_ERR, "Could not set PAM_USER.", pam_strerror(pamh, retval));
              closelog();
