7973ed5aecbaa459661415be4ef0a2769baec0a8
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2025-04-24 08:20:48 -0700

    fix: use pam_exec

--- files/scripts/setup-homefs.sh
+++ files/scripts/setup-homefs.sh
@@ -2,19 +2,10 @@
 
 set -oue pipefail
 
-OPEN_SES="session         required        pam_homefs.so mount"
-CLOSE_SES="session         required        pam_homefs.so unmount"
+OPEN_SES="session         required        pam_exec.so type=open /usr/libexec/homefs/manage_homedir --mount"
+CLOSE_SES="session         required        pam_exec.so type=close /usr/libexec/homefs/manage_homedir --unmount"
 
 echo "$OPEN_SES" >> /usr/share/authselect/default/sssd/postlogin
 echo "$CLOSE_SES" >> /usr/share/authselect/default/sssd/postlogin
 echo "$OPEN_SES" >> /usr/share/authselect/default/local/postlogin
 echo "$CLOSE_SES" >> /usr/share/authselect/default/local/postlogin
-
-dnf -y install make gcc pam-devel
-
-cd /usr/homefs-module/
-make install
-rm -rf /usr/homefs-module/
-cd /
-
-dnf -y remove make gcc pam-devel
