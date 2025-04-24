5e8be8109d8f8e5f7ad9fa74afe622aa367ce5d1
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2025-04-24 01:43:45 -0700

    feat: add custom PAM module

--- files/scripts/setup-homefs.sh
+++ files/scripts/setup-homefs.sh
@@ -2,12 +2,15 @@
 
 set -oue pipefail
 
-OPEN_SES="session     required type=open_session                   pam_exec.so /usr/libexec/homefs/manage_homedir	--mount"
-CLOSE_SES="session     required type=close_session                  pam_exec.so /usr/libexec/homefs/manage_homedir	--umount"
+OPEN_SES="session         required        pam_homefs.so mount"
+CLOSE_SES="session         required        pam_homefs.so unmount"
 
 echo "$OPEN_SES" >> /usr/share/authselect/default/sssd/postlogin
 echo "$CLOSE_SES" >> /usr/share/authselect/default/sssd/postlogin
 echo "$OPEN_SES" >> /usr/share/authselect/default/local/postlogin
 echo "$CLOSE_SES" >> /usr/share/authselect/default/local/postlogin
 
-authselect apply-changes
+cd /tmp/homefs_module
+make install
+rm -rf /tmp/homefs_module
+cd /
