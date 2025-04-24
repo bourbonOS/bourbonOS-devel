1c8cccb34a2c169157956a4bce781862a11980c3
certifiedfoolio <mastersvanity@gmail.com>
2025-04-23 23:06:08 -0700

    chore: organize modules

--- files/scripts/quirks.sh
+++ files/scripts/quirks.sh
@@ -3,3 +3,14 @@
 set -oue pipefail
 
 echo "$(date +%s)" > /etc/last_update_run
+chmod +x -R /tmp/osrelease/*
+
+if cat /etc/os-release | grep 'ID="fedora"' > /dev/null; then
+    /tmp/misc/image-info-stable.sh
+elif cat /etc/os-release | grep 'ID="centos"' > /dev/null; then
+    /tmp/misc/image-info-lts.sh
+    /tmp/misc/repo-setup-lts.sh
+else
+    echo "Unknown OS type. Abort."
+    exit 1
+fi
