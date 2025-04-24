769fa8d51fb51b83b271ec908052bda9c88539cf
certifiedfoolio <mastersvanity@gmail.com>
2025-04-23 23:43:38 -0700

    fix: integrate scripts directly

--- files/misc/repo-setup-lts.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -oue pipefail
-
-dnf -y install epel-release 'dnf-command(config-manager)'
-crb enable
-rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-10
--- files/scripts/quirks.sh
+++ files/scripts/quirks.sh
@@ -4,12 +4,48 @@ set -oue pipefail
 
 echo "$(date +%s)" > /etc/last_update_run
 
-if cat /etc/os-release | grep 'ID="fedora"' > /dev/null; then
-    /tmp/image-info-stable.sh
-elif cat /etc/os-release | grep 'ID="centos"' > /dev/null; then
-    /tmp/image-info-lts.sh
-    /tmp/repo-setup-lts.sh
+if cat /etc/os-release | grep 'ID="centos"' > /dev/null; then
+    IMAGE_PRETTY_NAME="bourbonOS $(cat /etc/bourbonOS_version)"
+    IMAGE_LIKE="centos"
+    HOME_URL="https://github.com/bourbonOS/bourbonOS-devel"
+    DOCUMENTATION_URL="https://github.com/bourbonOS/bourbonOS-devel"
+    SUPPORT_URL="https://github.com/bourbonOS/bourbonOS-devel/issues"
+    BUG_SUPPORT_URL="https://github.com/bourbonOS/bourbonOS-devel/issues"
+
+    sed -i "s/^VARIANT_ID=.*/VARIANT_ID=$IMAGE_NAME/" /usr/lib/os-release
+    sed -i "s/^PRETTY_NAME=.*/PRETTY_NAME=\"${IMAGE_PRETTY_NAME} (powered by CentOS Stream)\"/" /usr/lib/os-release
+    sed -i "s/^NAME=.*/NAME=\"$IMAGE_PRETTY_NAME\"/" /usr/lib/os-release
+    sed -i "s|^HOME_URL=.*|HOME_URL=\"$HOME_URL\"|" /usr/lib/os-release
+    sed -i "s|^DOCUMENTATION_URL=.*|DOCUMENTATION_URL=\"$DOCUMENTATION_URL\"|" /usr/lib/os-release
+    sed -i "s|^SUPPORT_URL=.*|SUPPORT_URL=\"$SUPPORT_URL\"|" /usr/lib/os-release
+    sed -i "s|^BUG_REPORT_URL=.*|BUG_REPORT_URL=\"$BUG_SUPPORT_URL\"|" /usr/lib/os-release
+    sed -i "s|^CPE_NAME=\"cpe:/o:fedoraproject:fedora|CPE_NAME=\"cpe:/o:secureblue:${IMAGE_PRETTY_NAME,}|" /usr/lib/os-release
+    sed -i "s/^DEFAULT_HOSTNAME=.*/DEFAULT_HOSTNAME=\"${IMAGE_PRETTY_NAME,}\"/" /usr/lib/os-release
+    sed -i "s/^ID=fedora/ID=\"${IMAGE_LIKE,}\"\nID_LIKE=\"${IMAGE_PRETTY_NAME,}\"/" /usr/lib/os-release
+    sed -i "/^REDHAT_BUGZILLA_PRODUCT=/d; /^REDHAT_BUGZILLA_PRODUCT_VERSION=/d; /^REDHAT_SUPPORT_PRODUCT=/d; /^REDHAT_SUPPORT_PRODUCT_VERSION=/d" /usr/lib/os-release
+    sed -i "s/^EFIDIR=.*/EFIDIR=\"fedora\"/" /usr/sbin/grub2-switch-to-blscfg
+
+    dnf -y install epel-release 'dnf-command(config-manager)'
+    crb enable
+    rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-10
 else
-    echo "Unknown OS type. Abort."
-    exit 1
+    IMAGE_PRETTY_NAME="bourbonOS $(cat /etc/bourbonOS_version)"
+    IMAGE_LIKE="fedora"
+    HOME_URL="https://github.com/bourbonOS/bourbonOS-devel"
+    DOCUMENTATION_URL="https://github.com/bourbonOS/bourbonOS-devel"
+    SUPPORT_URL="https://github.com/bourbonOS/bourbonOS-devel/issues"
+    BUG_SUPPORT_URL="https://github.com/bourbonOS/bourbonOS-devel/issues"
+
+    sed -i "s/^VARIANT_ID=.*/VARIANT_ID=$IMAGE_NAME/" /usr/lib/os-release
+    sed -i "s/^PRETTY_NAME=.*/PRETTY_NAME=\"${IMAGE_PRETTY_NAME} (powered by Fedora Atomic)\"/" /usr/lib/os-release
+    sed -i "s/^NAME=.*/NAME=\"$IMAGE_PRETTY_NAME\"/" /usr/lib/os-release
+    sed -i "s|^HOME_URL=.*|HOME_URL=\"$HOME_URL\"|" /usr/lib/os-release
+    sed -i "s|^DOCUMENTATION_URL=.*|DOCUMENTATION_URL=\"$DOCUMENTATION_URL\"|" /usr/lib/os-release
+    sed -i "s|^SUPPORT_URL=.*|SUPPORT_URL=\"$SUPPORT_URL\"|" /usr/lib/os-release
+    sed -i "s|^BUG_REPORT_URL=.*|BUG_REPORT_URL=\"$BUG_SUPPORT_URL\"|" /usr/lib/os-release
+    sed -i "s|^CPE_NAME=\"cpe:/o:fedoraproject:fedora|CPE_NAME=\"cpe:/o:secureblue:${IMAGE_PRETTY_NAME,}|" /usr/lib/os-release
+    sed -i "s/^DEFAULT_HOSTNAME=.*/DEFAULT_HOSTNAME=\"${IMAGE_PRETTY_NAME,}\"/" /usr/lib/os-release
+    sed -i "s/^ID=fedora/ID=\"${IMAGE_LIKE,}\"\nID_LIKE=\"${IMAGE_PRETTY_NAME,}\"/" /usr/lib/os-release
+    sed -i "/^REDHAT_BUGZILLA_PRODUCT=/d; /^REDHAT_BUGZILLA_PRODUCT_VERSION=/d; /^REDHAT_SUPPORT_PRODUCT=/d; /^REDHAT_SUPPORT_PRODUCT_VERSION=/d" /usr/lib/os-release
+    sed -i "s/^EFIDIR=.*/EFIDIR=\"fedora\"/" /usr/sbin/grub2-switch-to-blscfg
 fi
