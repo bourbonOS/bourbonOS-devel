b3c939c2b2622cf34cc3a75045eee20f11fcdcad
certifiedfoolio <mastersvanity@gmail.com>
2025-04-23 23:13:08 -0700

    fix: tmpdir refs

--- files/scripts/quirks.sh
+++ files/scripts/quirks.sh
@@ -3,7 +3,7 @@
 set -oue pipefail
 
 echo "$(date +%s)" > /etc/last_update_run
-chmod +x -R /tmp/osrelease/*
+chmod +x -R /tmp/misc/*
 
 if cat /etc/os-release | grep 'ID="fedora"' > /dev/null; then
     /tmp/misc/image-info-stable.sh
