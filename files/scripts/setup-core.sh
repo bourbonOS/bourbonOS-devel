ead29c223db02f6d09ec66d4f32f7f833567369f
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2025-05-07 11:52:45 -0700

    feat(WIP): integrate Restic for automatic backups

--- files/scripts/setup-core.sh
+++ files/scripts/setup-core.sh
@@ -3,14 +3,15 @@
 set -oue pipefail
 shopt -s extglob
 
+systemctl daemon-reload
+systemctl enable subsystem.service
+
 echo "session         required        pam_transit.so" >> /usr/share/authselect/default/sssd/postlogin
 echo "session         required        pam_transit.so" >> /usr/share/authselect/default/local/postlogin
 
 touch /etc/transit/transit-id
 chown root:root /etc/transit/transit-id
 chmod 700 /etc/transit/transit-id
-
-mkdir -p /usr/etc/skel/.ssh/
 
 dnf -y install make gcc pam-devel
 
