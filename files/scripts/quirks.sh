b56c5f466699d4c824a6b64523998fea49be609f
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2025-08-15 13:09:47 -0700

    fix: remove didyoumean refs

--- files/scripts/quirks.sh
+++ files/scripts/quirks.sh
@@ -49,5 +49,6 @@ elif grep -q "ID=fedora" /etc/os-release; then
     sed -i "/^REDHAT_BUGZILLA_PRODUCT=/d; /^REDHAT_BUGZILLA_PRODUCT_VERSION=/d; /^REDHAT_SUPPORT_PRODUCT=/d; /^REDHAT_SUPPORT_PRODUCT_VERSION=/d" /usr/lib/os-release
     sed -i "s/^EFIDIR=.*/EFIDIR=\"fedora\"/" /usr/sbin/grub2-switch-to-blscfg
 
+    # tempfix for dnf module failing
     dnf5 -y install 'dnf5-command(config-manager)'
 fi
