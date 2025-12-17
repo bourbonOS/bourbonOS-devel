4e3014349609ac2f07a0d34db91eaac3c002937c
somebody once told me <156134535+certifiedfoolio@users.noreply.github.com>
2025-12-16 18:29:50 -0800

    fix subsystem setup

--- files/scripts/setup-subsystem.sh
+++ files/scripts/setup-subsystem.sh
@@ -4,4 +4,4 @@ set -oue pipefail
 
 useradd --system --no-create-home --shell /usr/sbin/nologin subsys
 
-curl -Lo /usr/local/share/containers/default.container https://github.com/bourbonOS/cherries/releases/download/container-v1/default.container
+curl -Lo /usr/local/share/containers/default.container https://github.com/bourbonOS/cherries/releases/download/container-v2/default.container
